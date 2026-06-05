6e927e3d92f1bec6b4e0d58c5a82e59a6299ec84
jedzqer <jedzouquan@gmail.com>
2026-06-05 14:11:01 +0800

    1.20.6更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.safe.vision"
         minSdk = 24
         targetSdk = 36
-        versionCode = 49
-        versionName = "1.20.5"
+        versionCode = 50
+        versionName = "1.20.6"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
